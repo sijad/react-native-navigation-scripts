8e80db0bd9901c6d57b0fa6668827197140779fb
Sajjad Hashemian <wolaws@gmail.com>
2019-08-02 18:30:56 +0430

    change screen registration pattern

--- packages/react-native-template-rnn-typescript/src/commons/screens.ts
+++ packages/react-native-template-rnn-typescript/src/commons/screens.ts
@@ -2,11 +2,6 @@ import { Navigation } from 'react-native-navigation';
 import * as screens from '../screens';
 
 export function registerScreens() {
-  Object.keys(screens).forEach(screenKey => {
-    Navigation.registerComponent(screenKey, () => {
-      // register your Provider here
-      // TODO add example
-      return screens[screenKey as keyof typeof screens];
-    });
-  });
+  Navigation.registerComponent('Other', () => screens.Other);
+  Navigation.registerComponent('Home', () => screens.Home);
 }
