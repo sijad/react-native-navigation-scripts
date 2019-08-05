3d245782b265293ad21dac35d4aa800eb36ee677
Sajjad Hashemian <wolaws@gmail.com>
2019-08-05 14:23:41 +0430

    add template generator

--- packages/rnn-cli/lib/bin.js
+++ packages/rnn-cli/lib/bin.js
@@ -1,1 +1,22 @@
-console.log('WIP');
+#!/usr/bin/env node
+const { runner } = require('hygen');
+const Logger = require('hygen/lib/logger');
+const path = require('path');
+const fs = require('fs');
+
+const defaultTemplates = path.join(__dirname, '..', 'templates');
+const projectPath = path.join(__dirname, '..', '..', '..');
+
+const isTs = fs.existsSync(path.join(projectPath, 'tsconfig.json'));
+
+runner([isTs ? 'ts' : 'js'].concat(process.argv.slice(2)), {
+  templates: defaultTemplates,
+  cwd: projectPath,
+  logger: new Logger(console.log.bind(console)),
+  createPrompter: () => require('enquirer'),
+  exec: (action, body) => {
+    const opts = body && body.length > 0 ? { input: body } : {};
+    return require('execa').shell(action, opts);
+  },
+  debug: !!process.env.DEBUG,
+});
---
+++ packages/rnn-cli/templates/ts/screen/prompt.js
@@ -0,0 +1,7 @@
+module.exports = [
+  {
+    type: 'input',
+    name: 'screenName',
+    message: "What's your new screen name?",
+  },
+];
