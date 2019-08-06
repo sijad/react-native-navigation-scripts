02402c1291c49c7ea8dc13e4b5aaabc903ad2692
Sajjad Hashemian <wolaws@gmail.com>
2019-08-06 14:15:20 +0430

    use npm scripts to install rnn

--- packages/react-native-template-rnn-typescript/setup.js
+++ packages/react-native-template-rnn-typescript/setup.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const fs = require('fs');
-const execFileSync = require('child_process').execFileSync;
 
 const projectPath = path.join(__dirname, '..', '..');
 
@@ -10,9 +9,6 @@ const templateFilesToDelete = [
   'README.md',
   'LICENSE',
 ];
-
-const rnnPath = path.join(__dirname, '..', 'rnn-cli', 'lib', 'bin.js');
-execFileSync('node', [rnnPath, 'install', 'rnn'], { cwd: projectPath, env: { HYGEN_OVERWRITE: true } });
 
 const deleteFile = filePath => {
   if (!fs.existsSync(filePath)) {
